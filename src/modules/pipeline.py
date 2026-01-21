e7a38c1702a019defd189ec67c37a2085dd6ae78
K-Grachev-2106756 <m2106756@edu.misis.ru>
2026-01-21 19:57:19 +0300

    fix: pipeline.fit

--- src/modules/pipeline.py
+++ src/modules/pipeline.py
@@ -1,6 +1,7 @@
 import os
 import json
 from itertools import product
+from typing import Union
 
 import numpy as np
 
@@ -9,22 +10,11 @@ from .base import SparseModule, DenseModule
 
 class Pipeline:
 
-    def __init__(self, module_list: list[SparseModule]):
+    def __init__(self, module_list: list[Union[SparseModule, DenseModule]]):
         self.modules = module_list
         self.best_params = [None] * len(module_list)
         self.fit_logs = []
 
-    def _extract_pairs(self, X: list[str], y: np.ndarray):
-        pairs = []
-        labels = []
-        n = len(X)
-        for i in range(n):
-            for j in range(i + 1, n):
-                if not np.isnan(y[i, j]):
-                    pairs.append((X[i], X[j]))
-                    labels.append(int(y[i, j]))
-        return pairs, labels
-    
 
     def predict(self, X: list[str]):
         ans = np.ones(shape=(len(X), len(X)))
@@ -70,18 +60,6 @@ class Pipeline:
     
 
     def fit(self, X, y):
-        # Обучение dense модулей
-        all_pairs = []
-        all_labels = []
-        for x_batch, y_batch in zip(X, y):
-            pairs, labels = self._extract_pairs(x_batch, y_batch)
-            all_pairs.extend(pairs)
-            all_labels.extend(labels)
-
-        for module in self.modules:
-            if isinstance(module, DenseModule) and not module.is_fitted():
-                module.fit(all_pairs, all_labels)
-
         # Grid Search
         all_logits = [[module.get_logits(x) for module in self.modules] for x in X]  # Собираем логиты один раз сразу
         
